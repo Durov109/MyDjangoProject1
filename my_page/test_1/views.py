3bca50b7abb3781c31d6559d0a52c46ed21f679e
Dmitry <stonycc@yandex.ru>
2023-12-31 23:22:40 +0300

    .

--- my_page/test_1/views.py
+++ my_page/test_1/views.py
@@ -6,12 +6,31 @@ from test_1.models import Worker
 # Create your views here.
 
 
-# def index_page(request):
-#     all_workers = Worker.objects.all()
-#     print(all_workers)
+def index_page(request):
+    # Вывести все данные из таблицы
+    all_workers = Worker.objects.all()
+    print(all_workers)
+
+    # Фильтр по зарплате
+    worker_filtered_salary = Worker.objects.filter(salary=60000)
+    print(worker_filtered_salary)
+
+    # Запись в БД
+    new_worker = Worker(name='Иван', surmane='Иванов', salary=70000)
+    new_worker.save()
+    # или
+    Worker.objects.create(name='Иван', surmane='Иванов', salary=70000)
+
+    # Изменение уже существующей записи
+    worker_to_change = Worker.object.get(id=1)
+    worker_to_change.surname = 'Суворов'
+    worker_to_change.save()
+    # или
+    Worker.objects.filter(id=1).update(name='Суворов')
+
+    # Удаление записи
+    Worker.objects.get(id=1).delete()
 
 
 def hello_world(request):
-    all_workers = Worker.objects.all()
-    print(all_workers)
     return render(request, 'test_index.html')
